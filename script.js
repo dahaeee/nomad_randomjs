53f74407895ce884fb3a964a3f38a2d8cd4c766a
dahaeee <33ekgo@naver.com>
2022-06-18 22:39:04 +0900

    no message

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const result = document.querySelector("#result");
 const HIDDEN_CLASSNAME = "hidden";
 
 function printResult(input, random) {
-    if (input === random) {
+    if (parseInt(input) === parseInt(random)) {
         result.classList.remove(HIDDEN_CLASSNAME);
         result.innerText = "You won!😆";
     } else {
@@ -26,4 +26,38 @@ function guessNum(event) {
 }
 
 submit.addEventListener("click", guessNum);
+
+
+
+// const generateNumber = document.querySelector("#generate-number");
+// const guessNumber = document.querySelector("#guess-number");
+// const playbtn = document.querySelector("#play");
+// const resultText = document.querySelector(".result span");
+// const resultWin = document.querySelector(".result h3");
+// const NUMBERKEY = "machine number";
+// const USERNUMBERKEY = "user number";
+
+// function updateNumber() {
+//     const currentNumber = Math.ceil(Math.random() * generateNumber.value);
+//     const userNumber = guessNumber.value;
+//     localStorage.setItem(NUMBERKEY, currentNumber);
+//     localStorage.setItem(USERNUMBERKEY, userNumber);
+// }
+
+// setInterval(updateNumber, 500);
+
+// function printGameText(e) {
+//     e.preventDefault();
+//     let yourNumber = localStorage.getItem(USERNUMBERKEY);
+//     let pageNumber = localStorage.getItem(NUMBERKEY);
+//     resultText.innerText = `You chose: ${yourNumber}, the machine chose: ${pageNumber}`;
+
+//     if (yourNumber === pageNumber) {
+//         resultWin.innerText = "You won!";
+//     } else {
+//         resultWin.innerText = "You lost!";
+//     }
+// }
+
+// playbtn.addEventListener("click", printGameText);
 
